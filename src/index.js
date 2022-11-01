40abfecef038c612a3a53d818e442dc615be0d1b
sunandasharma61 <sunandasharma60101@gmail.com>
2022-11-01 20:28:10 +0530

    commting again

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://user2001:dAtAbase2001user@cluster0.kcc7xh8.mongodb.net/Sunanda2001-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
+
+
+  
 
 app.use('/', route);
 
