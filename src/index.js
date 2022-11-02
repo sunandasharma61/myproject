066459401144b7c27e6b8c668173a3c0aed3d1cb
sunandasharma61 <sunandasharma60101@gmail.com>
2022-11-02 21:47:51 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://user2001:dAtAbase2001user@cluster0.kcc7xh8.mongodb.net/Sunanda2001-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
